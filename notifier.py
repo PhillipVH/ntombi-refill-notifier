4eff97219e1d116b17367aa7be98e3da07c3f356
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-06-08 15:46:57 +0200

    Initial commit

---
+++ notifier.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+
+account_sid = "AC01b9db6780e4c48742ddd13434b292fe"
+auth_token = "ecc5932e2b26b029fd6530ff016f5cd6"
+
+TWILIO_NUMBER = "+16197176634"
+TARGET_NUMBER = "+27798792873"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+        to=TARGET_NUMBER,
+        from_=TWILIO_NUMBER,
+        body="Refill dispenser!")
+
+print(message.sid)
